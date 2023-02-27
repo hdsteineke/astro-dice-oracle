d5d8c7314f4d72dfd1a79d4517cd902b06c0b681
HAILEY STEINEKE <hailey.steineke@gmail.com>
2023-02-27 14:11:54 -0800

    successfully setting horoscope question from input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const [zodiacDie, setZodiacDie] = useState('');
+  const [planetDie, setPlanetDie] = useState('');
+  const [houseDie, setHouseDie] = useState('');
+  const [horoscope, setHoroscope] = useState('');
+
+  async function handleDiceRoll(e) {
+    e.preventDefault();
+    setQuestion(e.target.value);
+  }
+
+  async function handleSaveHoroscope() {
+
+  }
+
   return (
     <div className="App">
       <h1>Consult the Oracle:</h1>
@@ -9,22 +24,29 @@ function App() {
         <label>
           Ask a question: 
         </label>
-        <input type="text"></input>
+        <input value={question} onChange={e => setQuestion(e.target.value)}></input>
         <div className="dice-container">
           <div className="zodiac-dice">Zodiac</div>
           <div className="zodiac-dice">Planet</div>
           <div className="zodiac-dice">House</div>
         </div>
-        <button>Roll the dice</button>
+        <button onSubmit={handleDiceRoll()}>Roll the dice</button>
       </section>
       <section className="horoscope-template">
+        <h3>{question}</h3>
         <p>There once was a <span>______</span> that lived in the sky and fell in love with a <span>______</span>. He decided he loved it so much he wanted to live there and build his own <span>______</span>.
         </p>
       </section>
       <section className="horoscope-log">
-        <div className="horoscope-entry">One</div>
-        <div className="horoscope-entry">Two</div>
-        <div className="horoscope-entry">Three</div>
+        <div className="horoscope-entry">
+          <h4>(entry 1)</h4>
+        </div>
+        <div className="horoscope-entry">
+          <h4>(entry 2)</h4>
+        </div>
+        <div className="horoscope-entry">
+        <h4>(entry 3)</h4>
+        </div>
       </section>
     </div>
   );
